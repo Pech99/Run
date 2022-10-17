1877b0ad4ac683626d11365d399d8ee5f09b9941
Pecce <vittorio1999@live.com>
2022-10-17 23:30:16 +0200

    working

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module golang.org/run
+module github.com/Pech99/Run
 
 go 1.19
+
+require golang.org/x/sys v0.1.0 // indirect
--- run.go
+++ run.go
@@ -1,11 +1,33 @@
 package main
 
-func main() {
-	w23.getSelectedWindows()
+import (
+	"fmt"
+	"os"
 
+	sys "golang.org/x/sys/windows"
+)
+
+func main() {
+
+	for {
+		process, err := os.FindProcess(int(sys.GetForegroundWindow()))
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		fmt.Println(process)
+	}
 }
 
 func selectRoutin() []string {
 
 	return nil
+
 }
+
+/*
+C:\Users\Vitto\go\pkg\src\golang.org\x\sys
+C:\Users\Vitto\go\src\golang.org\x\sys
+C:\Program Files\Go\src\golang.org\x\sys
+*/
